12e04186da1d029053acf2f3d6cdc446a04c320e
Mahendra Pabbathi <pabbathimahendra8@gmail.com>
2025-06-18 10:37:21 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,51 @@
+import "dotenv/config"
+import express from "express"
+import cors from "cors"
+import http from "http"
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/UserRoutes.js";
+import messageRouter from "./routes/MessageRoutes.js";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app)
+
+// Initialize Socket.io
+export const io = new Server(server,{
+    cors:{origin:"*"}
+})
+
+// store online users
+export const userSocketMap = {}; // {userId : socketId}
+
+// Socket.io connection handler
+io.on("connection",(socket)=>{
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected",userId);
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    // emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", ()=>{
+        console.log("User Disconnected",userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    })
+})
+
+// middleware
+app.use(express.json({limit:"4mb"}))
+app.use(cors())
+
+app.use('/api/status',(req,res)=>res.send("Server is live"));
+app.use('/api/auth',userRouter);
+app.use('/api/messages',messageRouter);
+
+await connectDB();
+
+console.log("✅ Loaded Cloudinary API Key:", process.env.CLOUDINARY_API_KEY);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT,()=>console.log("Server is running on PORT: "+PORT))
